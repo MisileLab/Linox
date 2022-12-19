1ba703660264e9df3cd5751d47f3cc1ace67044c
misilelaboratory <misileminecord@gmail.com>
2022-12-20 00:32:34 +0900

    Some(update)

---
+++ installer.lua
@@ -0,0 +1,9 @@
+local shell = require("shell")
+
+function DownloadLinox(file)
+    shell.run("wget", {"https://raw.githubusercontent.com/MisileLab/Linox/main" .. file})
+end
+
+DownloadLinox("install.lua")
+shell.run("lua", {"install.lua"})
+DownloadLinox("main.lua")
--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 local basalt = require("basalt")
+local term = require("term")
 
-function sizer(perx, pery):
+function Sizer(perx, pery)
     local xy = term.getSize()
     return {math.floor(xy[0] / 100 * perx), math.floor(xy[1] / 100 * pery)}
 end
 
-print("_" * sizer(100, 100)[0])
-print("|" + (sizer(100, 100)[0] - 2) * " " + "|")
+print("_" * Sizer(100, 100)[0])
+print("|" + (Sizer(100, 100)[0] - 2) * " " + "|")
